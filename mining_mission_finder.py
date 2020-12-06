6232ae36b5a4db02eaf15c94bea49cc25a22942b
Jordan Wallace <39134950+jordicious@users.noreply.github.com>
2020-12-05 23:04:12 -0500

    lite mode

--- mining_mission_finder.py
+++ mining_mission_finder.py
@@ -3,21 +3,13 @@ import datetime as date
 from datetime import datetime
 import tkinter as tk
 
-
-master = tk.Tk()
-master.geometry("250x500")
-master.resizable(0, 1)
-master.title("Mining Missions")
-back = tk.Frame(master, bg='black')
-back.propagate(0)
-back.pack(fill=tk.BOTH, expand=1)
-
 econExclude = ["Extraction", "Refinery"]
 typeExclude = ["Fleet Carrier", "Planetary Outpost", "Planetary Port", "Outpost"]
 stateExclude = ["Civil War, Pirate Attack, Election"]
 
-with open("api.txt", "r") as Keys:
-    cmdr, api = (i.strip("\n") for i in Keys.readlines())
+with open("config.txt", "r") as Keys:
+    cmdr, api, lite = (i.strip("\n") for i in Keys.readlines())
+    lite = int(lite)
     Keys.close()
 radius = input("Radius >")
 
@@ -34,17 +26,28 @@ def timeSinceRefresh():
     return time, delta, time-delta
 
 
-t = tk.Text(master=back, height=100, width=500, back="black", fg='white')
-t.configure(font=("Arial", 12, "bold"))
-t.place(x=0, y=25)
+if not lite:
+    master = tk.Tk()
+    master.geometry("250x500")
+    master.resizable(0, 1)
+    master.title("Mining Missions")
+    back = tk.Frame(master, bg='black')
+    back.propagate(0)
+    back.pack(fill=tk.BOTH, expand=1)
+    t = tk.Text(master=back, height=100, width=500, back="black", fg='white')
+    t.configure(font=("Arial", 12, "bold"))
+    t.place(x=0, y=25)
 
 
 def update():
+    if not lite:
+        t.delete("1.0", "end")
+
     lastRefresh = timeSinceRefresh()
     print("Last Refresh: {} ({} ago)".format(lastRefresh[2], lastRefresh[1]))
 
     tracking = {}
-    t.delete("1.0", "end")
+
     if sysOverride:
         lastSys = sysOverride
     else:
@@ -79,7 +82,7 @@ def update():
         if sysInfo["factionState"] in stateExclude:
             continue
 
-        print("{} ({}): {}".format(i, k, econ) + ("/{}".format(econ2) if econ2 else ""))
+        # print("{} ({}): {}".format(i, k, econ) + ("/{}".format(econ2) if econ2 else ""))
 
         stations = requests.get("https://www.edsm.net/api-system-v1/stations", params={"systemName": i}).json()["stations"]
 
@@ -93,26 +96,34 @@ def update():
             # if int(j["distanceToArrival"]) > 15000:
                 # continue
 
+            # print("    {} ({}".format(j["name"], j["economy"]) + (", {})".format(j["secondEconomy"]) if j["secondEconomy"] else ")"))
 
-            print("    {} ({}".format(j["name"], j["economy"]) + (", {})".format(j["secondEconomy"]) if j["secondEconomy"] else ")"))
+            if not lite:
+                try:
+                    tracking[i].append(j["name"])
+                    t.insert("end", "    {}\n".format(j["name"]))
+                    print("    {}".format(j["name"]))
+                except KeyError:
+                    tracking[i] = [j["name"]]
+                    t.insert("end", "{} ({}ly)\n".format(i, k))
+                    t.insert("end", "    {}\n".format(j["name"]))
+                    print("{} ({}ly)\n    {}".format(i, k, j["name"]))
+                master.update()
 
-            try:
-                tracking[i].append(j["name"])
-                t.insert("end", "    {}\n".format(j["name"]))
-            except KeyError:
-                tracking[i] = [j["name"]]
-                t.insert("end", "{} ({}ly)\n".format(i, k))
-                t.insert("end", "    {}\n".format(j["name"]))
+            else:
+                try:
+                    tracking[i].append(j["name"])
+                    print("    {}".format(j["name"]))
+                except KeyError:
+                    tracking[i] = [j["name"]]
+                    print("{} ({}ly)\n    {}".format(i, k, j["name"]))
 
-        master.update()
-
-
-    print(tracking)
     if not len(tracking):
         t.insert("end", "None Found")
 
 
-b = tk.Button(master=back, text="Refresh", width=25, height=1, command=update).place(x=0, y=0)
+if not lite:
+    b = tk.Button(master=back, text="Refresh", width=25, height=1, command=update).place(x=0, y=0)
+    tk.mainloop()
 
-
-tk.mainloop()
+update()
